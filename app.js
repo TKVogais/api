7443a7b9543b1125df2d9426568d87a87ba4e3b7
TKVogais <jgparanagua@gmail.com>
2022-05-05 23:29:00 -0300

    Versão 1.1.1

--- app.js
+++ app.js
@@ -6,15 +6,10 @@ const cors = require('cors');
 
 
 //Configurações
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-    app.use(cors());
-    next();
-})
+
 const PORT = process.env.PORT || 3000;
 
 //Carregando Rotas
